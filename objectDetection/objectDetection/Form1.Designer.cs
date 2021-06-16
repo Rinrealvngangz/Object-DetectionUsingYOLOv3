1116789623b00190091fdbcaff70132697d7ffd3
Rinrealvngangz <tuannmctk42@gmail.com>
2021-06-16 23:37:04 +0700

    add feature realtime detec

--- objectDetection/objectDetection/Form1.Designer.cs
+++ objectDetection/objectDetection/Form1.Designer.cs
@@ -31,7 +31,10 @@
             this.btnImage = new System.Windows.Forms.Button();
             this.btnDetection = new System.Windows.Forms.Button();
             this.picBoxImageForDetec = new System.Windows.Forms.PictureBox();
+            this.pictureBoxVideo = new System.Windows.Forms.PictureBox();
+            this.playVideo = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.picBoxImageForDetec)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxVideo)).BeginInit();
             this.SuspendLayout();
             // 
             // btnImage
@@ -58,16 +61,36 @@
             // 
             this.picBoxImageForDetec.Location = new System.Drawing.Point(14, 61);
             this.picBoxImageForDetec.Name = "picBoxImageForDetec";
-            this.picBoxImageForDetec.Size = new System.Drawing.Size(808, 372);
+            this.picBoxImageForDetec.Size = new System.Drawing.Size(485, 372);
             this.picBoxImageForDetec.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.picBoxImageForDetec.TabIndex = 2;
             this.picBoxImageForDetec.TabStop = false;
+            // 
+            // pictureBoxVideo
+            // 
+            this.pictureBoxVideo.Location = new System.Drawing.Point(539, 61);
+            this.pictureBoxVideo.Name = "pictureBoxVideo";
+            this.pictureBoxVideo.Size = new System.Drawing.Size(789, 546);
+            this.pictureBoxVideo.TabIndex = 3;
+            this.pictureBoxVideo.TabStop = false;
+            // 
+            // playVideo
+            // 
+            this.playVideo.Location = new System.Drawing.Point(1033, 15);
+            this.playVideo.Name = "playVideo";
+            this.playVideo.Size = new System.Drawing.Size(75, 23);
+            this.playVideo.TabIndex = 4;
+            this.playVideo.Text = "Play video";
+            this.playVideo.UseVisualStyleBackColor = true;
+            this.playVideo.Click += new System.EventHandler(this.playVideo_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(847, 485);
+            this.ClientSize = new System.Drawing.Size(1340, 619);
+            this.Controls.Add(this.playVideo);
+            this.Controls.Add(this.pictureBoxVideo);
             this.Controls.Add(this.picBoxImageForDetec);
             this.Controls.Add(this.btnDetection);
             this.Controls.Add(this.btnImage);
@@ -75,6 +98,7 @@
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.picBoxImageForDetec)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxVideo)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -84,6 +108,8 @@
         private System.Windows.Forms.Button btnImage;
         private System.Windows.Forms.Button btnDetection;
         private System.Windows.Forms.PictureBox picBoxImageForDetec;
+        private System.Windows.Forms.PictureBox pictureBoxVideo;
+        private System.Windows.Forms.Button playVideo;
     }
 }
 
--- objectDetection/objectDetection/Form1.cs
+++ objectDetection/objectDetection/Form1.cs
@@ -4,11 +4,17 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using Alturos.Yolo;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
 namespace objectDetection
 {
     public partial class Form1 : Form
@@ -21,12 +27,94 @@ namespace objectDetection
         const string UNKOWN = "unknown key";
         const string PERSON = "person";
         string filePath = "";
+      
         int countCar, countBicycle, countBus, countMoto, countTruck, countTrain = 0;
 
+        private void playVideo_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "(mp3,wav,mp4,mov,wmv,mpg)|*.mp3;*.wav;*.mp4;*.mov;*.wmv;*.mpg|all files|*.*";
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+              string  filePathVideo = openFile.FileName;
+                CaptureVideo(filePathVideo);
+            }
+                
+        }
+        Bitmap BitmapFromSource(BitmapSource bitmapsource)
+        {
+            Bitmap bitmap;
+            using (MemoryStream outStream = new MemoryStream())
+            {
+                BitmapEncoder enc = new BmpBitmapEncoder();
+                enc.Frames.Add(BitmapFrame.Create(bitmapsource));
+                enc.Save(outStream);
+                bitmap = new Bitmap(outStream);
+            }
+            return bitmap;
+        }
+        void CaptureVideo(string filePathVideo)
+        {
+            // YOLO setting
+            int yoloWidth = 608, yoloHeight = 608;
+          //  var configurationDetector = new YoloConfigurationDetector();
+           // var config = configurationDetector.Detect();
+            YoloWrapper yoloWrapperVideo = new YoloWrapper("yolov3.cfg", "yolov3.weights", "coco.names");
 
+            VideoCapture videocapture;
+            Mat image = new Mat();
+            WriteableBitmap wb = new WriteableBitmap(yoloWidth, yoloHeight, 96, 96, PixelFormats.Bgr24, null);
+
+            byte[] imageInBytes = new byte[(int)(yoloWidth * yoloHeight * image.Channels())];
+            using (videocapture = new VideoCapture(filePathVideo))
+            {
+                int sleepTime = (int)Math.Round(1000 / videocapture.Fps);
+                using (Mat imageOriginal = new Mat())
+                    {
+                    while (true)
+                    {
+                        // read a single frame and convert the frame into a byte array
+                        videocapture.Read(imageOriginal);
+
+                        image = imageOriginal.Resize(new OpenCvSharp.Size(yoloWidth, yoloHeight));
+                        if (image.Empty())
+                            break;
+                        imageInBytes = image.ToBytes();
+
+                        // conduct object detection and display the result
+                        var items = yoloWrapperVideo.Detect(imageInBytes);
+
+                        foreach (var item in items)
+                        {
+                            if (item.Type != UNKOWN && item.Type != PERSON)
+                            {
+                                var x = item.X;
+                                var y = item.Y;
+                                var width = item.Width;
+                                var height = item.Height;
+                                var type = item.Type;  // class name of the object
+
+                                // draw a bounding box for the detected object
+                                // you can set different colors for different classes
+                                Cv2.Rectangle(image, new OpenCvSharp.Rect(x, y, width, height), Scalar.Green, 3);
+                            }
+                        }
+                        
+                    
+
+                        // display the detection result
+                        WriteableBitmapConverter.ToWriteableBitmap(image, wb);
+
+                        pictureBoxVideo.Image = BitmapFromSource(wb);
+                        Cv2.WaitKey(sleepTime);
+                    }
+                }
+               
+            }
+        }
         void OpenImage()
         {
-
+            
             OpenFileDialog openFile = new OpenFileDialog();
             openFile.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
             if (openFile.ShowDialog() == DialogResult.OK)
@@ -60,6 +148,7 @@ namespace objectDetection
             //  var config = configurationDetector.Detect();
             using (var yoloWrapper = new YoloWrapper("yolov3.cfg", "yolov3.weights", "coco.names"))
             {
+                
                 var items = yoloWrapper.Detect(filePath);
 
                 var image = Image.FromFile(filePath);
@@ -98,13 +187,13 @@ namespace objectDetection
                                     break;
                             }
 
-                            using (var pen = new Pen(Color.Red, 3))
+                            using (var pen = new System.Drawing.Pen(System.Drawing.Color.Red, 3))
                             {
                                 grap.DrawRectangle(pen, item.X, item.Y, item.Width, item.Height);
                                 Font font = new Font("Meiryo UI", 15);
-                                SolidBrush brush = new SolidBrush(Color.Green);
+                                SolidBrush brush = new SolidBrush(System.Drawing.Color.Green);
                                 grap.DrawString(item.Type.ToString(), font, brush, item.X, item.Y);
-                                grap.DrawString(Math.Round(item.Confidence * 100).ToString() + "%", new Font("Meiryo UI", 25), new SolidBrush(Color.Purple), item.X + (item.Width - 50), item.Y + (item.Height / 2));
+                                grap.DrawString(Math.Round(item.Confidence * 100).ToString() + "%", new Font("Meiryo UI", 25), new SolidBrush(System.Drawing.Color.Purple), item.X + (item.Width - 50), item.Y + (item.Height / 2));
                                 grap.Flush();
                             }
                         }
@@ -118,27 +207,27 @@ namespace objectDetection
                         {
 
                             case "car":
-                                grap.DrawString(listVehicle[i] + ": " + countCar, new Font("Meiryo UI", 24), new SolidBrush(Color.Red), 0, index);
+                                grap.DrawString(listVehicle[i] + ": " + countCar, new Font("Meiryo UI", 24), new SolidBrush(System.Drawing.Color.Red), 0, index);
                                 index += 40;
                                 break;
                             case "bus":
-                                grap.DrawString(listVehicle[i] + ": " + countBus, new Font("Meiryo UI", 24), new SolidBrush(Color.Red), 0, index);
+                                grap.DrawString(listVehicle[i] + ": " + countBus, new Font("Meiryo UI", 24), new SolidBrush(System.Drawing.Color.Red), 0, index);
                                 index += 40;
                                 break;
                             case "bicycle":
-                                grap.DrawString(listVehicle[i] + ": " + countBicycle, new Font("Meiryo UI", 24), new SolidBrush(Color.Red), 0, index);
+                                grap.DrawString(listVehicle[i] + ": " + countBicycle, new Font("Meiryo UI", 24), new SolidBrush(System.Drawing.Color.Red), 0, index);
                                 index += 40;
                                 break;
                             case "motorbike":
-                                grap.DrawString(listVehicle[i] + ": " + countMoto, new Font("Meiryo UI", 24), new SolidBrush(Color.Red), 0, index);
+                                grap.DrawString(listVehicle[i] + ": " + countMoto, new Font("Meiryo UI", 24), new SolidBrush(System.Drawing.Color.Red), 0, index);
                                 index += 40;
                                 break;
                             case "train":
-                                grap.DrawString(listVehicle[i] + ": " + countTrain, new Font("Meiryo UI", 24), new SolidBrush(Color.Red), 0, index);
+                                grap.DrawString(listVehicle[i] + ": " + countTrain, new Font("Meiryo UI", 24), new SolidBrush(System.Drawing.Color.Red), 0, index);
                                 index += 40;
                                 break;
                             case "truck":
-                                grap.DrawString(listVehicle[i] + ": " + countTruck, new Font("Meiryo UI", 24), new SolidBrush(Color.Red), 0, index);
+                                grap.DrawString(listVehicle[i] + ": " + countTruck, new Font("Meiryo UI", 24), new SolidBrush(System.Drawing.Color.Red), 0, index);
                                 index += 40;
                                 break;
                             default:
